f5d0f8074bb0978b82afef755e07d28ee783bfa7
Stuart Nelson <stuartjnelson@users.noreply.github.com>
2024-12-17 22:45:32 +0000

    updating setup

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,6 @@ export interface projectConfig {
 export const config:projectConfig = {
     storageType: process.env.STORAGE_TYPE || 'local', // 'local' | 'netlify' 
     localFilePath: './release-data.json',             // Local file path
-    gitRepoPath: `${import.meta.dirname}/reposToScan/northern-badger-11ty-blog`
+    gitRepoPath: `${import.meta.dirname}/../reposToScan/northern-badger-11ty-blog`
 };
   
