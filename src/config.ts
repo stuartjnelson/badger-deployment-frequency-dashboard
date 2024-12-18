743ceb6c7b4809b6ffbed9c3fbe03b6e7c6ab4c6
Stuart Nelson <stuartjnelson@users.noreply.github.com>
2024-12-18 13:37:10 +0000

    refactor: removing old prop

--- src/config.ts
+++ src/config.ts
@@ -2,14 +2,12 @@ export type commitMessageIdentifierType = 'conventionalCommits' | 'githubMergeRe
 
 export interface projectConfig {
     storageType: string | 'local' | 'netlify',
-    localFilePath: string,
     gitRepoPath: string,
     commitMessageIdentifierType: commitMessageIdentifierType
 }
 
 export const config:projectConfig = {
     storageType: process.env.STORAGE_TYPE || 'local', // 'local' | 'netlify' 
-    localFilePath: './release-data.json',             // Local file path
     gitRepoPath: `${import.meta.dirname}/../reposToScan/northern-badger-11ty-blog`,
     commitMessageIdentifierType: 'githubMergeRequest'
     // commitMessageIdentifierType: 'conventionalCommits'
